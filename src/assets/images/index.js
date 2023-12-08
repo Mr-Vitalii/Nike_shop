5568f7399abe39c838df86763f328f09ff4f578b
Mr-Vitalii <co4ckowoi.v@gmail.com>
2023-12-08 22:07:38 +0200

    created hero section

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,8 +1,22 @@
 
 import headerLogo from './header-logo.svg'
+import bigShoe1 from './big-shoe1.png'
+import bigShoe2 from './big-shoe2.png'
+import bigShoe3 from './big-shoe3.png'
 
+import thumbnailBackground from './thumbnail-background.svg'
+import thumbnailShoe1 from './thumbnail-shoe1.svg'
+import thumbnailShoe2 from './thumbnail-shoe2.svg'
+import thumbnailShoe3 from './thumbnail-shoe3.svg'
 
 
 export {
-    headerLogo
+    headerLogo,
+    bigShoe1,
+    bigShoe2,
+    bigShoe3,
+    thumbnailBackground,
+    thumbnailShoe1,
+    thumbnailShoe2,
+    thumbnailShoe3
 }
