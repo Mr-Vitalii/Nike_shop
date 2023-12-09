44e0f08314bd0dcdcfec54400d12ae75728c75df
Mr-Vitalii <co4ckowoi.v@gmail.com>
2023-12-09 20:40:59 +0200

    created popular-products section

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -9,14 +9,27 @@ import thumbnailShoe1 from './thumbnail-shoe1.svg'
 import thumbnailShoe2 from './thumbnail-shoe2.svg'
 import thumbnailShoe3 from './thumbnail-shoe3.svg'
 
+import shoe4 from './shoe4.svg'
+import shoe5 from './shoe5.svg'
+import shoe6 from './shoe6.svg'
+import shoe7 from './shoe7.svg'
+import shoe8 from './shoe8.svg'
+
 
 export {
     headerLogo,
     bigShoe1,
     bigShoe2,
     bigShoe3,
+
     thumbnailBackground,
     thumbnailShoe1,
     thumbnailShoe2,
-    thumbnailShoe3
+    thumbnailShoe3,
+
+    shoe4,
+    shoe5,
+    shoe6,
+    shoe7,
+    shoe8,
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 
-import { bigShoe1, bigShoe2, bigShoe3, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3 } from "../assets/images";
+import { bigShoe1, bigShoe2, bigShoe3, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3, shoe4, shoe5, shoe6, shoe7, } from "../assets/images";
 
 
 export const navLinks = [
@@ -27,6 +27,29 @@ export const shoes = [
     {
         thumbnail: thumbnailShoe3,
         bigShoe: bigShoe3,
+    },
+];
+
+export const products = [
+    {
+        imgURL: shoe4,
+        name: "Nike Air Jordan-01",
+        price: "$200.20",
+    },
+    {
+        imgURL: shoe5,
+        name: "Nike Air Jordan-10",
+        price: "$210.20",
+    },
+    {
+        imgURL: shoe6,
+        name: "Nike Air Jordan-100",
+        price: "$220.20",
+    },
+    {
+        imgURL: shoe7,
+        name: "Nike Air Jordan-001",
+        price: "$230.20",
     },
 ];
 
