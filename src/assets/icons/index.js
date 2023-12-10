fd04fa86a3c73b21287d8cfb3888579244981c19
Mr-Vitalii <co4ckowoi.v@gmail.com>
2023-12-10 12:55:20 +0200

    created a Services section

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,10 +2,16 @@
 import hamburger from './hamburger.svg'
 import arrowRight from './arrow-right.svg'
 import star from './star.svg'
+import support from './support.svg'
+import shieldTick from './shield-tick.svg'
+import truckFast from './truck-fast.svg'
 
 
 export {
     hamburger,
     arrowRight,
-    star
+    star,
+    support,
+    shieldTick,
+    truckFast
 }
